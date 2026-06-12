ea6c83df6041ccc4ad6efe0a7765b3ea3aabfb36
Helena Rosa <helena.rosa@aluno.senai.br>
2026-06-12 15:41:58 -0300

    valores posiyivos e negativos

--- src/components/Contador/Contador.jsx
+++ src/components/Contador/Contador.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import styles from "./Contador.module.css"
 function Contador() {
     const [contador, setContador] = useState(0);
    
@@ -18,23 +19,40 @@ function Contador() {
         }
 
         const sortear= () =>{
-            setContador(Math.random);
+            setContador(Math.floor (Math.random() * (100 - 1 )));
+        }
+
+
+        const valorClasse = () => {
+            if (contador > 0) return styles.valorPositivo
+            if (contador < 0) return styles.valorNegativo
+        }
+
+        const formatarContador = () => {
+            if (contador >= 0 && contador < 10) {
+                return `0${contador}`;
+
+            }
+
+            return contador;
         }
 
     return (
-        <>
-            <div>
-                <p> {contador}</p>
-                <button onClick={reiniciar}>Reiniciar</button>
+        <>  
+       
+            <div className={styles.formatarContador}>
+                 <p className={valorClasse()}> {contador}</p>
+                
+                <button className= {styles.button1} onClick={reiniciar}>Reiniciar</button>
 
                 
-                <button onClick={somar}>Somar Numero</button>
+                <button className= {styles.button2}onClick={somar}>Somar Numero</button>
 
                
-                <button onClick={subtrair}>Subtrair Numero</button>
+                <button className= {styles.button3} onClick={subtrair}>Subtrair Numero</button>
 
                 
-                <button onClick={sortear}>Sortear</button>
+                <button className= {styles.button4} onClick={sortear}>Sortear</button>
 
             </div>
         </>
