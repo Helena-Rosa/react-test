e63761e7eee665be654399b26d8bb6256b18a33a
Helena Rosa <helena.rosa@aluno.senai.br>
2026-08-20 17:29:04 -0300

    finalizamos consultas e produtos, precisso so dicionar o localstorage e target

--- src/components/Produtos/Consulta.jsx
+++ src/components/Produtos/Consulta.jsx
@@ -1,14 +1,40 @@
-function Consulta (){
-   
-    return (
-         <main>
-        <h1></h1>
-    </main>
+import { useState, useEffect } from "react";
+import Produto from "./Produto";
+
+function Consulta() {
+// Estado que vai controlar a renderização do produto na UI
+  
+
+// Inserir o produto no Local Storage
+  useEffect(() => {
+
+    
+  }, []);
+
+// Recuperar o produto do Local Storage quando o produto for modificado pelo estado
+  useEffect(() => {
+    
+  }, []);
+
+// Extrair o texto do Botão quando o usuário clicar
+  function handleClick() {
+    
+  }
+
+  return (
+    <section>
+      <h1>Preferência:</h1>
+
+      <button onClick={handleClick}>notebook</button>
+
+      <button onClick={handleClick}>smartphone</button>
+
+      <Produto />
+
+    </section>
+  );
 
 
-    );
 }
 
-
-
-export default Consulta; 
+export default Consulta;
